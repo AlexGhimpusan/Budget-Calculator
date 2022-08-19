ac7a21d294fef9276579335ba656d6ad48c3c364
Alexandru Ghimpușan <ghimpusan_alexandru@yahoo.com>
2022-08-19 20:26:58 +0300

    Add the control flow of the calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ControlComponent } from './components/control/control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlComponent
   ],
   imports: [
     BrowserModule
